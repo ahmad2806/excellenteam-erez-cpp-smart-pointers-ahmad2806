fcffe9b1a622e4008899cdb64ab4249ba61f8e7c
ahmad2806 <ahmadhashem2806@gmail.com>
2018-12-09 22:11:32 +0200

    testing was added

--- SharedPtr/SharedPtr.h
+++ SharedPtr/SharedPtr.h
@@ -6,6 +6,9 @@
 
 #define EXCELLENTEAM_EREZ_CPP_SMART_POINTERS_AHMAD2806_SHAREDPTR_H
 
+#include "gtest/gtest.h"
+#include <iostream>
+
 template<typename T>
 class SharedPtr {
 public:
@@ -36,13 +39,13 @@ private:
 
 template<typename T>
 SharedPtr<T>::SharedPtr(T *ptr): m_ptr(ptr) {
-    m_counter = new(size_t);
-    *m_counter = 0;
+    m_counter = new size_t;
+    *m_counter = 1;
 }
 
 template<typename T>
 SharedPtr<T>::~SharedPtr() {
-    *m_counter--;
+    --(*m_counter);
 
     if (!*m_counter) {
         delete m_ptr;
@@ -69,9 +72,12 @@ SharedPtr<T>::operator bool() const {
 template<typename T>
 SharedPtr<T> &SharedPtr<T>::operator=(T *ptr) {
     if (m_ptr != ptr) {
-        *m_counter--;
-        if (!m_counter)
+        --(*m_counter);
+        if (!*m_counter) {
             delete m_ptr;
+            delete m_counter;
+        }
+        m_counter = new size_t;
         *m_counter = 1;
         m_ptr = ptr;
     }
@@ -88,22 +94,132 @@ template<typename T>
 SharedPtr<T>::SharedPtr(SharedPtr const &other) {
     m_counter = other.m_counter;
     m_ptr = other.m_ptr;
-    *m_counter++;
+    ++(*m_counter);
 }
 
 template<typename T>
 SharedPtr<T> &SharedPtr<T>::operator=(SharedPtr const &other) {
-    m_counter --;
+    --(*m_counter);
 
-    if (!*m_counter)
-        ~SharedPtr();
+    if (!*m_counter) {
+        delete m_ptr;
+        delete m_counter;
+    }
 
     m_counter = other.m_counter;
     m_ptr = other.m_ptr;
-    *m_counter++;
+    ++(*m_counter);
 
     return *this;
 
 }
 
+
+TEST(SharedPointerBasicTests, AddressTest
+) {
+int *p = new int;
+SharedPtr<int> tmp_ptr(p);
+ASSERT_TRUE (p
+== tmp_ptr.
+
+get()
+
+);
+}
+
+TEST(SharedPointerBasicTests, ArrowOperatorTest
+) {
+SharedPtr<std::string> tmp_ptr(new std::string);
+*
+tmp_ptr = "testing";
+ASSERT_TRUE (tmp_ptr
+->compare("testing") == 0);
+}
+
+TEST(SharedPointerBasicTests, StarOperatorTest
+) {
+int *p = new int;
+*
+p = 5;
+SharedPtr<int> tmp_ptr(p);
+ASSERT_TRUE (*p
+== *tmp_ptr);
+
+}
+
+TEST(SharedPointerBasicTests, BoolCastTest
+) {
+SharedPtr<std::string> tmp_ptr(new std::string);
+*
+tmp_ptr = "testing";
+
+ASSERT_TRUE(bool(tmp_ptr));
+
+SharedPtr<std::string> tmp_ptr2(new std::string);
+tmp_ptr2 = NULL;
+
+ASSERT_FALSE(bool(tmp_ptr2));
+
+}
+
+TEST(SharedPointerBasicTests, GetTest
+) {
+int *p = new int;
+SharedPtr<int> tmp_ptr(p);
+ASSERT_EQ (tmp_ptr
+.
+
+get(), p
+
+);
+
+}
+
+TEST(SharedPointerBasicTests, AssignmentFromPointerTest
+) {
+int *p = new int;
+SharedPtr<int> tmp_ptr(p);
+int *p1 = new int;
+*
+p1 = 5;
+tmp_ptr = p1;
+ASSERT_EQ (p1, tmp_ptr
+.
+
+get()
+
+);
+
+}
+
+TEST(SharedPointerBasicTests, AssignmentFromSharedPointerTest
+) {
+int *p = new int;
+*
+p = 6;
+SharedPtr<int> tmp_ptr(p);
+int *p1 = new int(5);
+SharedPtr<int> tmp_ptr1(p1);
+tmp_ptr = tmp_ptr1;
+ASSERT_EQ (tmp_ptr1
+.
+
+get(), tmp_ptr
+
+.
+
+get()
+
+);
+
+}
+
+TEST(SharedPointerBasicTests, CopyCtorSharedPointerTest) {
+int *p = new int;
+*p = 6;
+SharedPtr<int> tmp_ptr(p);
+SharedPtr<int> tmp_ptr1(tmp_ptr);
+ASSERT_EQ (tmp_ptr1.get(), tmp_ptr.get());
+
+}
 #endif //EXCELLENTEAM_EREZ_CPP_SMART_POINTERS_AHMAD2806_SHAREDPTR_H
---
+++ SharedPtr/sharedPtr_test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ahmad on 09/12/2018.
+//
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "SharedPtr.h"
+
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
